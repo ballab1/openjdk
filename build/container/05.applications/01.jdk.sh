94cda55170b8e6e28534a9bfd405fc1a886ec9cb
Bob Ballantyne <bob.ballantyne@emc.com>
2018-02-11 13:08:39 -0500

    updated jdk installation

--- build/container/05.applications/01.jdk.sh
+++ build/container/05.applications/01.jdk.sh
@@ -5,8 +5,8 @@ set -o errexit
 set -o nounset
 #set -o xtrace
 
-export JAVA_HOME=/usr/lib/jvm/java-1.8-openjdk/jre
-export PATH="${PATH}:/usr/lib/jvm/java-1.8-openjdk/jre/bin"
+export JAVA_HOME=/usr/lib/jvm/java-1.8-openjdk
+export PATH="${PATH}:/usr/lib/jvm/java-1.8-openjdk/bin"
 
 declare JAVA_VERSION=8u131
 declare JAVA_ALPINE_VERSION=8.131.11-r2
